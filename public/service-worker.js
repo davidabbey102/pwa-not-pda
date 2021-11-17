0e2e2e3c4c8c5021f7df4c69cde28960116671ee
David Abbey <davidabbey@earthlink.net>
2021-11-17 14:19:56 -0800

    everything works locally, just no install option in address bar

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,14 +12,19 @@ const FILES_TO_CACHE = [
     "/manifest.webmanifest"
 ];
 
+// install
 self.addEventListener("install", function (evt) {
+    // pre cache image data
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
-            return cache.addAll(FILES_TO_CACHE);
-        })
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
     );
 
+    // pre cache all static assets
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
     self.skipWaiting();
 });
 
@@ -51,7 +56,6 @@ self.addEventListener("fetch", function (evt) {
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
-
                         return response;
                     })
                     .catch(err => {
@@ -64,14 +68,9 @@ self.addEventListener("fetch", function (evt) {
     }
 
     evt.respondWith(
-        fetch(evt.request).catch(function () {
-            return caches.match(evt.request).then(function (response) {
-                if (response) {
-                    return response;
-                } else if (evt.request.headers.get("accept").includes("text/html")) {
-                    // return the cached home page for all requests for html pages
-                    return caches.match("/");
-                }
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
             });
         })
     );
